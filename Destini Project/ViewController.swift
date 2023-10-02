af09a797ff47e3c8a27bd1f157f788e6de51b567
frio anbia <frioanbia63@gmail.com>
2023-10-02 13:45:07 +0700

    Initial Commit

---
+++ Destini Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini Project
+//
+//  Created by Frio Anbia on 02/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
