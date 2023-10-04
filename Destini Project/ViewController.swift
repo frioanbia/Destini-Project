3dc31ada15013854fd4e98476bfbf11fb70b3873
frio anbia <frioanbia63@gmail.com>
2023-10-04 08:35:27 +0700

    preview folder

--- Destini Project/ViewController.swift
+++ Destini Project/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrian = StroyBrian()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrian.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrian.getStoryTitle()
+        choice1Button.setTitle(storyBrian.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrian.getChoice2(), for: .normal)
+    }
+    
 }
 
